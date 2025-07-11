c3cede029abac640b22bba19650a91632f33638f
Juliana <julianafigueirac@gmail.com>
2025-07-10 23:39:24 -0300

    criando a conexão com o gemini

--- script.js
+++ script.js
@@ -5,12 +5,41 @@ const askButton = document.getElementById('askButton')
 const aiResponse = document.getElementById('aiResponse')
 const form = document.getElementById('form')
 
-const perguntarAI = async () => {
 
+
+const askAI = async (question, game, apiKey) => {
+    const model = "gemini-2.5-flash"
+    const geminiURL = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${apiKey}`
+
+    const pergunta = `
+    
+    `
+
+    const contents = [{
+        parts: [{
+            text: pergunta
+        }]
+    }]
+
+    // chamada API
+    const response = await fetch(geminiURL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'aplication/json'
+        },
+        body: JSON.stringify({
+            contents
+        })
+    })
+
+    const data = await response.json()
+
+    console.log({ data })
+    return data.candidates[0].content.parts[0].text
 }
 
 
-const sendForm = (event) => {
+const sendForm = async (event) => {
     event.preventDefault()
     const apiKey = apiKeyInput.value
     const game = gameSelect.value
@@ -29,7 +58,8 @@ const sendForm = (event) => {
 
     try {
         // perguntar ia
-
+        const text = await askAI(question, game, apiKey)
+        aiResponse.querySelector('.response-content')
 
     } catch (error) {
         console.log('Erro:', error)
